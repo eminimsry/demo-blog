faf1bfb74be13c7687954dd750afc40a1cccea78
eminimsry <1004805444@qq.com>
2020-04-02 19:24:09 +0800

    Revert "我是项目经理，我提交了错误代码"
    This reverts commit c53f00ab

--- src/main/java/com/demo/blog/aspect/LogAspect.java
+++ src/main/java/com/demo/blog/aspect/LogAspect.java
@@ -17,7 +17,6 @@ import java.util.Arrays;
 public class LogAspect {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    //我是项目经理，不对啊，老弟
     @Pointcut("execution(* com.demo.blog.controller.*.*(..))")
     public void log() {
     }
